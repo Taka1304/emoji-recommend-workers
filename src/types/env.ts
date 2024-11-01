53e274cabb26354bc6e2862722251e8f3e8890e0
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-11-01 11:38:23 +0900

    ♻️ refactor: 環境変数はcf-typegenで生成できるらしい

--- src/types/env.ts
+++
@@ -1,9 +0,0 @@
-import type { SlackEdgeAppEnv } from "slack-cloudflare-workers";
-
-export type Env = SlackEdgeAppEnv & {
-	D1: D1Database;
-	VECTORIZE: Vectorize;
-	HF_API_TOKEN: string;
-	CF_ACCOUNT_ID: string;
-	CF_AI_GATEWAY_ID: string;
-};
